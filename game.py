ed576939080fb9f21bbf1dd887f25112d8f5025d
Pranav Bawa <pb389@njit.com>
2021-04-24 16:13:23 -0700

    commit

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import random
 game_playing = False
+player_score = 0
+cpu_score = 0
 choice = ['Rock', 'Paper', 'Scissor']
 computer_choice = random.choice(choice)
 pick = choice[int(input("Please select your choice from below: "
@@ -7,4 +9,3 @@ pick = choice[int(input("Please select your choice from below: "
                         "\n1: Paper"
                         "\n2: Scissor"
                         "\n"))]
-
